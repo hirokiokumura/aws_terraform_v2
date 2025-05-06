68781f4b99e8d198a84f09a12f105798c6e5d515
Hiroki Okumura <ouji.info@gmail.com>
2025-05-06 15:35:54 +0900

    test

--- terraform/athena.tf
+++ terraform/athena.tf
@@ -1,12 +1,20 @@
+data "aws_iam_account_alias" "current" {}
+
+locals {
+  account_alias = data.aws_iam_account_alias.current.account_alias
+  athena_engine_version = "Athena engine version 3"
+}
+
+
 resource "aws_athena_workgroup" "workgroup" {
-  name = "apricot1224-workgroup"
+  name = "${local.account_alias}"
 
   configuration {
     enforce_workgroup_configuration    = true
     publish_cloudwatch_metrics_enabled = true
 
     engine_version {
-      selected_engine_version = "Athena engine version 3"
+      selected_engine_version = local.athena_engine_version
     }
 
     result_configuration {
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.72.1"
+      version = "~> 5.97.0"
     }
   }
   backend "s3" {
-    bucket = "apricot1224-terraform"
+    bucket = "apricot1224v1-terraform"
     key    = "state/terraform.tfstate"
     region = "ap-northeast-1"
   }
