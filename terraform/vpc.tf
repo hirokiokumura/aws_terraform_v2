5ddcca80bfc3d3f1bf822728e06a1d1060480e79
Hiroki Okumura <ouji.info@gmail.com>
2025-05-25 13:26:25 +0900

    add vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -114,12 +114,7 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.primary.id
   service_name      = "com.amazonaws.ap-northeast-1.s3"
   vpc_endpoint_type = "Gateway"
-
-  subnet_ids = [
-    aws_subnet.primary_v1.id, aws_subnet.primary_v2.id
-  ]
-
   tags = {
-    "Name" = "primary-s3"
+    "Name" = "primary-gateway-s3"
   }
 }
