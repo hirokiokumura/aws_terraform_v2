93eadf41d3ab1d162acea48ade5b1be89089ea37
Hiroki Okumura <ouji.info@gmail.com>
2025-05-25 10:49:40 +0900

    add vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "primary" {
   cidr_block = "10.0.0.0/22"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags = {
     Name = "primary_vpc"
@@ -26,12 +28,12 @@ resource "aws_subnet" "primary_v2" {
 
 resource "aws_vpc_ipv4_cidr_block_association" "secondary" {
   vpc_id     = aws_vpc.primary.id
-  cidr_block = "10.0.4.0/24"
+  cidr_block = "10.1.0.0/24"
 }
 
 resource "aws_subnet" "secondary_v1" {
   vpc_id     = aws_vpc_ipv4_cidr_block_association.secondary.vpc_id
-  cidr_block = "10.0.4.0/25"
+  cidr_block = "10.1.4.0/25"
 
   tags = {
     Name = "subnet_secondary_v1"
@@ -40,7 +42,7 @@ resource "aws_subnet" "secondary_v1" {
 
 resource "aws_subnet" "secondary_v2" {
   vpc_id     = aws_vpc_ipv4_cidr_block_association.secondary.vpc_id
-  cidr_block = "10.0.4.128/25"
+  cidr_block = "10.1.4.128/25"
 
   tags = {
     Name = "subnet_secondary_v2"
