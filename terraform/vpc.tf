9623dd82a6a0931f037b3d696545b5049cf880e9
Hiroki Okumura <ouji.info@gmail.com>
2025-05-25 10:33:42 +0900

    add vpc

---
+++ terraform/vpc.tf
@@ -0,0 +1,48 @@
+resource "aws_vpc" "primary" {
+  cidr_block = "10.0.0.0/22"
+
+  tags = {
+    Name = "primary_vpc"
+  }
+}
+
+resource "aws_subnet" "primary_v1" {
+  vpc_id     = aws_vpc.primary.id
+  cidr_block = "10.0.0.0/23"
+
+  tags = {
+    Name = "subnet_primary_v1"
+  }
+}
+
+resource "aws_subnet" "primary_v2" {
+  vpc_id     = aws_vpc.primary.id
+  cidr_block = "10.0.2.0/23"
+
+  tags = {
+    Name = "subnet_primary_v2"
+  }
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "secondary" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24"
+}
+
+resource "aws_subnet" "secondary_v1" {
+  vpc_id     = aws_vpc_ipv4_cidr_block_association.secondary.vpc_id
+  cidr_block = "10.0.4.0/25"
+
+  tags = {
+    Name = "subnet_secondary_v1"
+  }
+}
+
+resource "aws_subnet" "secondary_v2" {
+  vpc_id     = aws_vpc_ipv4_cidr_block_association.secondary.vpc_id
+  cidr_block = "10.0.4.128/25"
+
+  tags = {
+    Name = "subnet_secondary_v2"
+  }
+}
