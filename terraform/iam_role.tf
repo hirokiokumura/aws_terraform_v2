ea3cda12d9b52114e118f3c78b4626a4ffdcbcb3
Hiroki Okumura <ouji.info@gmail.com>
2025-06-01 14:57:00 +0900

    aws_iam_role_policy

---
+++ terraform/iam_role.tf
@@ -0,0 +1,63 @@
+resource "aws_iam_role" "AutomationAssumeRole" {
+  name               = "AutomationAssumeRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ssm.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "config.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name        = "AutomationAssumeRole"
+    Environment = "Production"
+  }
+}
+
+resource "aws_iam_role_policy" "AutomationAssumeRolePolicy" {
+  name   = "AutomationAssumeRolePolicy"
+  role   = aws_iam_role.AutomationAssumeRole.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetBucketPolicy",
+          "s3:PutBucketPolicy",
+          "s3:GetBucketAcl",
+          "s3:PutBucketAcl"
+        ]
+        Resource = "arn:aws:s3:::*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:StartAutomationExecution",
+          "ssm:GetAutomationExecution",
+          "ssm:DescribeAutomationExecutions"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:PassRole"
+        ]
+        Resource = aws_iam_role.AutomationAssumeRole.arn
+      }
+    ]
+  })
+}
