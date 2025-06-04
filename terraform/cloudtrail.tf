d76a883e92896c859cbf92fdca9846fe7640ceac
Hiroki Okumura <ouji.info@gmail.com>
2025-06-05 08:53:30 +0900

    cloudtrail

--- terraform/cloudtrail.tf
+++ terraform/cloudtrail.tf
@@ -1,14 +1,14 @@
 data "aws_caller_identity" "current" {}
-data "aws_iam_account_alias" "current" {}
+# data "aws_iam_account_alias" "current" {}
 
 locals {
-  account_id    = data.aws_caller_identity.current.account_id
-  account_alias = data.aws_iam_account_alias.current.account_alias
+  # account_id    = data.aws_caller_identity.current.account_id
+  # account_alias = data.aws_iam_account_alias.current.account_alias
   bucket_name   = "cloudtrail"
 }
 
 module "cloudtrail" {
-  source = "../../modules/cloudtrail/"
+  source = "../modules/cloudtrail/"
 
   cloudtrail_name   = "${local.account_alias}-cloudtrail"
   bucket_name       = "${local.account_alias}-${local.bucket_name}"
