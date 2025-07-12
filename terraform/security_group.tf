c97badc08dd6102658ab04909482a0db1d966795
Hiroki Okumura <ouji.info@gmail.com>
2025-07-12 22:37:57 +0900

    egress

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -18,10 +18,10 @@ resource "aws_security_group_rule" "ingress_https" {
 }
 
 resource "aws_security_group_rule" "engress_https" {
-  type              = "engress"
+  type              = "egress"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
   security_group_id = aws_security_group.this.id
-  cidr_blocks       = ["aws_vpc.primary.cidr_block"]
+  cidr_blocks       = ["aws_subnet.primary_v1.cidr_block"]
 }
