c53071b2daab72e7c899a7e2e8b716897c813b46
Hiroki Okumura <ouji.info@gmail.com>
2025-07-12 21:49:14 +0900

    インスタンスプロファイル

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -62,3 +62,28 @@ resource "aws_iam_role_policy" "AutomationAssumeRolePolicy" {
     ]
   })
 }
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-example-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_attach_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-example-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
