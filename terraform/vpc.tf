fded53ba097d410177f3055483ac8a83d3a797f4
Hiroki Okumura <ouji.info@gmail.com>
2025-07-12 22:16:30 +0900

    add subnet

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -138,7 +138,7 @@ resource "aws_vpc_endpoint" "ssm" {
   private_dns_enabled = true
 
   subnet_ids = [
-    aws_subnet.primary_v1.id, aws_subnet.primary_v2.id
+    aws_subnet.primary_v1.id
   ]
 }
 
@@ -154,7 +154,7 @@ resource "aws_vpc_endpoint" "ssmmessages" {
   private_dns_enabled = true
 
   subnet_ids = [
-    aws_subnet.primary_v1.id, aws_subnet.primary_v2.id
+    aws_subnet.primary_v1.id
   ]
 }
 
@@ -170,6 +170,6 @@ resource "aws_vpc_endpoint" "ec2messages" {
   private_dns_enabled = true
 
   subnet_ids = [
-    aws_subnet.primary_v1.id, aws_subnet.primary_v2.id
+    aws_subnet.primary_v1.id
   ]
 }
