113cb7fd0ac878462d18b5b547708b1b3df24f77
Hiroki Okumura <ouji.info@gmail.com>
2025-07-12 21:54:13 +0900

    add private link

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -125,3 +125,39 @@ resource "aws_vpc_endpoint" "s3" {
     "Name" = "primary-gateway-s3"
   }
 }
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id            = aws_vpc.primary.id
+  service_name      = "com.amazonaws.ap-northeast-1.ssm"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.this.id,
+  ]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id            = aws_vpc.primary.id
+  service_name      = "com.amazonaws.ap-northeast-1.ssmmessages"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.this.id,
+  ]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_id            = aws_vpc.primary.id
+  service_name      = "com.amazonaws.ap-northeast-1.ec2messages"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.this.id,
+  ]
+
+  private_dns_enabled = true
+}
