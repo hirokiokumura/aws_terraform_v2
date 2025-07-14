9a17f82a9d9a50fba9810812c29b793ce927b09c
Hiroki Okumura <ouji.info@gmail.com>
2025-07-14 22:36:47 +0900

    egress

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -57,79 +57,79 @@ resource "aws_route_table_association" "assoc_subnet_primary_1c" {
   route_table_id = aws_route_table.rtb_subnet_primary_1c.id
 }
 
-# resource "aws_vpc_ipv4_cidr_block_association" "secondary" {
-#   vpc_id     = aws_vpc.primary.id
-#   cidr_block = "10.1.4.0/24"
-# }
+resource "aws_vpc_ipv4_cidr_block_association" "secondary" {
+  vpc_id     = aws_vpc.primary.id
+  cidr_block = "10.1.4.0/24"
+}
 
-# resource "aws_subnet" "secondary_1a" {
-#   vpc_id            = aws_vpc_ipv4_cidr_block_association.secondary.vpc_id
-#   availability_zone = "ap-northeast-1a"
-#   cidr_block        = "10.1.4.0/25"
+resource "aws_subnet" "secondary_1a" {
+  vpc_id            = aws_vpc_ipv4_cidr_block_association.secondary.vpc_id
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.1.4.0/25"
 
-#   tags = {
-#     Name = "subnet_secondary_1a"
-#   }
-# }
+  tags = {
+    Name = "subnet_secondary_1a"
+  }
+}
 
-# resource "aws_subnet" "secondary_1c" {
-#   vpc_id            = aws_vpc_ipv4_cidr_block_association.secondary.vpc_id
-#   availability_zone = "ap-northeast-1c"
-#   cidr_block        = "10.1.4.128/25"
-
-
-#   tags = {
-#     Name = "subnet_secondary_1c"
-#   }
-# }
-
-# resource "aws_route_table" "rtb_subnet_secondary_1a" {
-#   vpc_id = aws_vpc_ipv4_cidr_block_association.secondary.vpc_id
+resource "aws_subnet" "secondary_1c" {
+  vpc_id            = aws_vpc_ipv4_cidr_block_association.secondary.vpc_id
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.1.4.128/25"
 
 
-#   tags = {
-#     Name = "rtb-subnet_secondary_1a"
-#   }
-# }
+  tags = {
+    Name = "subnet_secondary_1c"
+  }
+}
 
-# resource "aws_route_table" "rtb_subnet_secondary_1c" {
-#   vpc_id = aws_vpc_ipv4_cidr_block_association.secondary.vpc_id
-
-#   tags = {
-#     Name = "rtb-subnet_secondary_1c"
-#   }
-# }
-
-# # サブネットとルートテーブルの紐付け
-# resource "aws_route_table_association" "assoc_subnet_secondary_1a" {
-#   subnet_id      = aws_subnet.secondary_1a.id
-#   route_table_id = aws_route_table.rtb_subnet_secondary_1a.id
-
-#   depends_on = [aws_route_table.rtb_subnet_secondary_1a]
-# }
+resource "aws_route_table" "rtb_subnet_secondary_1a" {
+  vpc_id = aws_vpc_ipv4_cidr_block_association.secondary.vpc_id
 
 
-# resource "aws_route_table_association" "assoc_subnet_secondary_1c" {
-#   subnet_id      = aws_subnet.secondary_1c.id
-#   route_table_id = aws_route_table.rtb_subnet_secondary_1c.id
-#   depends_on     = [aws_route_table.rtb_subnet_secondary_1c]
-# }
+  tags = {
+    Name = "rtb-subnet_secondary_1a"
+  }
+}
 
-# resource "aws_vpc_endpoint" "s3" {
-#   vpc_id            = aws_vpc.primary.id
-#   service_name      = "com.amazonaws.ap-northeast-1.s3"
-#   vpc_endpoint_type = "Gateway"
+resource "aws_route_table" "rtb_subnet_secondary_1c" {
+  vpc_id = aws_vpc_ipv4_cidr_block_association.secondary.vpc_id
 
-#   route_table_ids = [
-#     aws_route_table.rtb_subnet_primary_1a.id,
-#     aws_route_table.rtb_subnet_primary_1c.id,
-#     aws_route_table.rtb_subnet_secondary_1a.id,
-#     aws_route_table.rtb_subnet_secondary_1c.id
-#   ]
-#   tags = {
-#     "Name" = "primary-gateway-s3"
-#   }
-# }
+  tags = {
+    Name = "rtb-subnet_secondary_1c"
+  }
+}
+
+# サブネットとルートテーブルの紐付け
+resource "aws_route_table_association" "assoc_subnet_secondary_1a" {
+  subnet_id      = aws_subnet.secondary_1a.id
+  route_table_id = aws_route_table.rtb_subnet_secondary_1a.id
+
+  depends_on = [aws_route_table.rtb_subnet_secondary_1a]
+}
+
+
+resource "aws_route_table_association" "assoc_subnet_secondary_1c" {
+  subnet_id      = aws_subnet.secondary_1c.id
+  route_table_id = aws_route_table.rtb_subnet_secondary_1c.id
+  depends_on     = [aws_route_table.rtb_subnet_secondary_1c]
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.primary.id
+  service_name      = "com.amazonaws.ap-northeast-1.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [
+    aws_route_table.rtb_subnet_primary_1a.id,
+    aws_route_table.rtb_subnet_primary_1c.id,
+    aws_route_table.rtb_subnet_secondary_1a.id,
+    aws_route_table.rtb_subnet_secondary_1c.id
+  ]
+  tags = {
+    "Name" = "primary-gateway-s3"
+  }
+}
 
 # resource "aws_vpc_endpoint" "ssm" {
 #   vpc_id            = aws_vpc.primary.id
